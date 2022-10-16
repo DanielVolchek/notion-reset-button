60c44579d4845ade53aab1500ec818485a2b52d8
Daniel Volchek <danielvolchek123@gmail.com>
2022-10-16 18:43:39 -0400

    add

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,26 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import RippleButton from "../Button";
 
 const Home: NextPage = () => {
-  const pressedResetButton = async () => {
-    await fetch("/api/update");
-    console.log("done resetting");
+  // const pressedResetButton = async () => {
+  //   fetch("/api/update")
+  //     .then(() => console.log("sucessfully reset data"))
+  //     .catch((err) => console.log("failed to fetch data, err: ", err));
+  // };
+
+  const pressedResetButton = () => {
+    console.log("reset button pressed");
   };
 
   return (
-    <main className="flex h-screen w-full items-center justify-center bg-black ">
+    <main className="flex h-screen w-full items-center justify-center bg-[#212529] ">
       <button
         onClick={pressedResetButton}
-        className="border-none bg-blue-500 py-6 px-8 text-2xl shadow-md transition-transform duration-[250ms] hover:scale-105 hover:bg-blue-600"
+        className="border-none bg-blue-500 py-6
+        px-8
+        text-2xl
+        text-white shadow-md transition-all duration-[250ms] hover:scale-105 hover:bg-blue-600 active:bg-emerald-300 "
       >
         Press To Reset
       </button>
