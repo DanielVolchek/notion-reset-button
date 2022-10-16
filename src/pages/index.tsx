63f2fb25986ed2af1f3871c39f6f5c55b7d05f4b
Daniel Volchek <danielvolchek123@gmail.com>
2022-10-16 18:59:12 -0400

    add

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import RippleButton from "../Button";
 
 const Home: NextPage = () => {
   // const pressedResetButton = async () => {
